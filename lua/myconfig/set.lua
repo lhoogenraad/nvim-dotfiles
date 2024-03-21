87a81807d3df4bb0671bf0bb65eed952a6358493
lhoogenraad <le.o.n@outlook.com>
2024-03-22 11:14:44 +1300

    Fixed highlights

--- lua/myconfig/set.lua
+++ lua/myconfig/set.lua
@@ -7,5 +7,7 @@ vim.opt["shiftwidth"] = 4
 
 -- Some random commands for text colouring
 vim.cmd([[:highlight LeapBackDrop guifg=#f4cafc guibg=#222222]])
+vim.cmd([[:hi IncSearch guibg=#47030f guifg=#adad71]])
+vim.cmd([[:hi Search guibg=#6e0519 guifg=#adad71]])
 vim.cmd([[:highlight HighlightUndo guibg=#b8645e]])
 vim.cmd([[:highlight HighlightRedo guibg=#50913a]])
