8aca788cd217a1b16e9f91427844fd4def6a522d
lhoogenraad <le.o.n@outlook.com>
2024-03-28 14:42:36 +1300

    Colors

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -6,6 +6,11 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 	vim.cmd([[:highlight LeapBackDrop guifg=#f4cafc guibg=#222222]])
+	vim.cmd([[:hi IncSearch guibg=#47030f guifg=#adad71]])
+	vim.cmd([[:hi Search guibg=#6e0519 guifg=#adad71]])
+	vim.cmd([[:hi HighlightUndo guibg=#b8645e]])
+	vim.cmd([[:hi HighlightRedo guibg=#50913a]])
+	vim.cmd([[:hi ColorColumn guibg=#491896]])
 end
 
 require('hlargs').setup{
--- lua/myconfig/set.lua
+++ lua/myconfig/set.lua
@@ -3,12 +3,6 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.o.termguicolors = true
 vim.o.scrolloff = 10
+vim.opt.colorcolumn="80"
 vim.opt["tabstop"] = 4
 vim.opt["shiftwidth"] = 4
-
--- Some random commands for text colouring
-vim.cmd([[:highlight LeapBackDrop guifg=#f4cafc guibg=#222222]])
-vim.cmd([[:hi IncSearch guibg=#47030f guifg=#adad71]])
-vim.cmd([[:hi Search guibg=#6e0519 guifg=#adad71]])
-vim.cmd([[:highlight HighlightUndo guibg=#b8645e]])
-vim.cmd([[:highlight HighlightRedo guibg=#50913a]])
