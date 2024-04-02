b689124e7d59bc53844793fac45360f862c509ba
lhoogenraad <le.o.n@outlook.com>
2024-04-03 07:14:14 +1300

    Increased colorcolumn width

--- lua/myconfig/set.lua
+++ lua/myconfig/set.lua
@@ -3,6 +3,6 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.o.termguicolors = true
 vim.o.scrolloff = 10
-vim.opt.colorcolumn="80"
+vim.opt.colorcolumn="100"
 vim.opt["tabstop"] = 4
 vim.opt["shiftwidth"] = 4
