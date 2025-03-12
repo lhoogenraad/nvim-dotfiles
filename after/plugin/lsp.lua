1572c7bb4060a85e011fc4707d068fe5cd50e1a9
lhoogenraad <le.o.n@outlook.com>
2025-03-12 14:24:56 +1300

    feat: Improved hover help

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,8 @@ lsp_zero.on_attach(function(client, bufnr)
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<C-k>", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("i", "<C-k>", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -39,6 +41,7 @@ cmp.setup({
 	formatting = lsp_zero.cmp_format(),
 	mapping = cmp.mapping.preset.insert({
 		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-z>'] = cmp.mapping.select_prev_item(cmp_select),
 		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 		['<CR>'] = cmp.mapping.confirm({ select = true }),
 		['<C-Space>'] = cmp.mapping.complete(),
