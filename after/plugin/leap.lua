f14054391c5aca97079f052af82cf81d054f3867
Leon <leon.hoogenraad@tetherhq.com>
2026-01-28 09:15:26 +1300

    Update leap config to suppress warnings on startup

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -1,1 +1,5 @@
-require('leap').add_default_mappings()
+-- Leap (replacement for add_default_mappings / create_default_mappings)
+vim.keymap.set({ "n", "x", "o" }, "s",  "<Plug>(leap-forward)",      { silent = true })
+vim.keymap.set({ "n", "x", "o" }, "S",  "<Plug>(leap-backward)",     { silent = true })
+vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)",  { silent = true })
+
