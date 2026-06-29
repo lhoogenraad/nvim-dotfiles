43df0c044ddf28f751628e5ab39810cc260f72c9
Leon <leon@betorigami.com>
2026-06-30 09:44:30 +1200

    fixes to telescope versioning

--- lua/myconfig/Packer.lua
+++ lua/myconfig/Packer.lua
@@ -1,104 +1,146 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer
-	use 'wbthomason/packer.nvim'
+    -- Packer
+    use 'wbthomason/packer.nvim'
 
-	-- TS
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
-	use 'neovim/nvim-lspconfig'
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v4.x',
-		requires = {
-			--- Uncomment these if you want to manage LSP servers from neovim
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+    ------------------------------------------------------------------
+    -- Treesitter / LSP
+    ------------------------------------------------------------------
 
-			{'neovim/nvim-lspconfig'},
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+    }
 
-	-- git
-	use 'lewis6991/gitsigns.nvim'
-	use 'akinsho/git-conflict.nvim'
-	use 'tpope/vim-fugitive'
+    use 'neovim/nvim-lspconfig'
 
-	-- file/buffer navigation
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use 'ThePrimeagen/harpoon'
-	use 'https://codeberg.org/andyg/leap.nvim'
-	use 'mbbill/undotree'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x',
+        requires = {
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-	-- Colour themes
-	use 'NLKNguyen/papercolor-theme'
-	use "folke/tokyonight.nvim"
-	use 'ful1e5/onedark.nvim'
-	use 'folke/lsp-colors.nvim'
-	use 'folke/twilight.nvim'
+            { 'neovim/nvim-lspconfig' },
 
-	-- visual stuff
-	use	{
-		'willothy/veil.nvim',
-		requires = {
-			-- All optional, only required for the default setup.
-			-- If you customize your config, these aren't necessary.
-			"nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-file-browser.nvim"
-		}
-	}
-	use {
-		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
-	}
-	use 'windwp/windline.nvim'
-	use {
-		'numToStr/Comment.nvim',
-		config = function()
-			require('Comment').setup()
-		end
-	}
-	use 'nvim-tree/nvim-web-devicons'
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
 
-	-- dev support
-	use 'm-demare/hlargs.nvim'
-	use({
-		"kylechui/nvim-surround",
-		tag = "*", 
-		config = function()
-			require("nvim-surround").setup({})
-		end
-	})
-	use 'tzachar/highlight-undo.nvim'
-	use 'edluffy/specs.nvim'
-	use 'sontungexpt/stcursorword'
-	use 'ErichDonGubler/lsp_lines.nvim'
-	use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
-	use 'lewis6991/impatient.nvim'
-	use({
-		"utilyre/barbecue.nvim",
-		tag = "*",
-		requires = {
-			"SmiteshP/nvim-navic",
-			"nvim-tree/nvim-web-devicons", -- optional dependency
-		},
-		after = "nvim-web-devicons", -- keep this if you're using NvChad
-		config = function()
-			require("barbecue").setup()
-		end,
-	})
-	use 'github/copilot.vim'
-	use 'norcalli/nvim-colorizer.lua'
-	use 'j-hui/fidget.nvim'
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
+
+    ------------------------------------------------------------------
+    -- Git
+    ------------------------------------------------------------------
+
+    use 'lewis6991/gitsigns.nvim'
+    use 'akinsho/git-conflict.nvim'
+    use 'tpope/vim-fugitive'
+
+    ------------------------------------------------------------------
+    -- Navigation
+    ------------------------------------------------------------------
+
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+        }
+    }
+
+	use "ThePrimeagen/harpoon"
+
+    use 'https://codeberg.org/andyg/leap.nvim'
+    use 'mbbill/undotree'
+
+    ------------------------------------------------------------------
+    -- Themes
+    ------------------------------------------------------------------
+
+    use 'NLKNguyen/papercolor-theme'
+    use 'folke/tokyonight.nvim'
+    use 'ful1e5/onedark.nvim'
+    use 'folke/lsp-colors.nvim'
+    use 'folke/twilight.nvim'
+
+    ------------------------------------------------------------------
+    -- UI
+    ------------------------------------------------------------------
+
+    use {
+        'willothy/veil.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-telescope/telescope-file-browser.nvim',
+        }
+    }
+
+    use {
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup({})
+        end,
+    }
+
+    use 'windwp/windline.nvim'
+
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end,
+    }
+
+    use 'nvim-tree/nvim-web-devicons'
+
+    ------------------------------------------------------------------
+    -- Development
+    ------------------------------------------------------------------
+
+    use 'm-demare/hlargs.nvim'
+
+    use {
+        'kylechui/nvim-surround',
+        tag = '*',
+        config = function()
+            require('nvim-surround').setup({})
+        end,
+    }
+
+    use 'tzachar/highlight-undo.nvim'
+    use 'edluffy/specs.nvim'
+    use 'sontungexpt/stcursorword'
+    use 'ErichDonGubler/lsp_lines.nvim'
+
+    use {
+        'kevinhwang91/nvim-ufo',
+        requires = 'kevinhwang91/promise-async',
+    }
+
+    use {
+        'utilyre/barbecue.nvim',
+        tag = '*',
+        requires = {
+            'SmiteshP/nvim-navic',
+            'nvim-tree/nvim-web-devicons',
+        },
+        after = 'nvim-web-devicons',
+        config = function()
+            require('barbecue').setup()
+        end,
+    }
+
+    ------------------------------------------------------------------
+    -- Misc
+    ------------------------------------------------------------------
+
+    use 'github/copilot.vim'
+
+    use 'norcalli/nvim-colorizer.lua'
+
+    use 'j-hui/fidget.nvim'
+
 end)
--- lua/myconfig/init.lua
+++ lua/myconfig/init.lua
@@ -1,4 +1,3 @@
 require("myconfig.remap")
-require("impatient").enable_profile()
 require('Comment').setup()
 require("myconfig.set")
